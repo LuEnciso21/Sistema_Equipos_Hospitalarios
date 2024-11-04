e35c47e9cc812f0684c93c8a1a853f02b92ce341
LuEnciso21 <luisafernanda192002@hotmail.com>
2024-11-03 23:02:22 -0500

    inicio

---
+++ hce/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    documento = models.CharField(max_length=30)  # CharField para texto corto
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)  # EmailField es mejor para correos electrónicos
+    imagen = models.ImageField(upload_to='imagenes/', null=True)  
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Test(models.Model):
+    id = models.AutoField(primary_key=True)
+    documento = models.CharField(max_length=50)
+    testname = models.CharField(max_length=100)
+    testresult = models.IntegerField() 
+    def __str__(self):
+        return f"Test {self.testname} - {self.documento}"
+
+
+
+
